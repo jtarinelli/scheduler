bb1e6a0a6df54b4121594c5965d09e10249e1e0f
unknown <juliatarinelli4@gmail.com>
2020-11-25 15:53:06 -0500

    Break up some functions, FIFO prints to console

--- script.js
+++ script.js
@@ -11,21 +11,38 @@ function addJob() {
 	var job1 = document.getElementById("job-1");
 	var jobsList = document.getElementById("jobs");
 	var newJob = job1.cloneNode(true);
-	newJob.id = "job-" + numJobs;
-	newJob.children[0].innerText = "Job " + numJobs;
-	newJob.children[2].id = "start-" + numJobs;
-	newJob.children[2].value = 0;
-	newJob.children[1].setAttribute("for","start-" + numJobs);
-	newJob.children[4].id = "length-" + numJobs;
-	newJob.children[4].value = 0;
-	newJob.children[1].setAttribute("for","length-" + numJobs);
+	changeJobNumber(newJob, numJobs);
+	resetJob(newJob);
 	jobsList.appendChild(newJob);
+}
+
+// updates an existing job to change all references to its number
+function changeJobNumber(job, number) {
+	job.id = "job-" + number;
+	job.children[0].innerText = "Job " + number;
+	job.children[2].id = "start-" + number;
+	job.children[1].setAttribute("for","start-" + number);
+	job.children[4].id = "length-" + number;
+	job.children[1].setAttribute("for","length-" + number);
+}
+
+// changes all the inputs for a job to 0
+function resetJob(job) {
+	job.children[2].value = 0;
+	job.children[4].value = 0;
 }
 
 function submitButton() {
 	console.log("submit button pressed");
+	jobs = readInJobs();
+	console.log(jobs);
+	FIFO(jobs);
+}
+
+// reads in inputs from the form and returns a list of job objects
+function readInJobs() {
 	var jobsList = document.getElementById("jobs");
-	jobs = [];
+	var jobs = []
 	
 	for(job of jobsList.children) {
 		var name = job.children[0].innerText;
@@ -37,7 +54,21 @@ function submitButton() {
 			length, length
 		});
 	}
-	console.log(jobs);
+	return jobs;
+}
+
+function FIFO(jobs) {
+	jobs.sort(job => job.start); // check about compatability of arrow functions
+	var time = 0;
+	for (job of jobs) {
+		console.log(job.name + " runs at time " + time);
+		time += job.length; 
+	}
+	console.log("All jobs complete at time " + time);
+}
+
+function roundRobin(jobs) {
+	
 }
 
 function makeBlock() {
