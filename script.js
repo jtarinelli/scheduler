8006b3bd85a6d5a0f4ab02082ed784577d390c5a
unknown <juliatarinelli4@gmail.com>
2020-11-25 14:51:03 -0500

    Adding new jobs totally works

--- script.js
+++ script.js
@@ -12,18 +12,38 @@ function addJob() {
 	var jobsList = document.getElementById("jobs");
 	var newJob = job1.cloneNode(true);
 	newJob.id = "job-" + numJobs;
-	newJob.firstElementChild.innerText = "Job " + numJobs;
+	newJob.children[0].innerText = "Job " + numJobs;
+	newJob.children[2].id = "start-" + numJobs;
+	newJob.children[2].value = 0;
+	newJob.children[1].setAttribute("for","start-" + numJobs);
+	newJob.children[4].id = "length-" + numJobs;
+	newJob.children[4].value = 0;
+	newJob.children[1].setAttribute("for","length-" + numJobs);
 	jobsList.appendChild(newJob);
 }
 
-function submit() {
+function submitButton() {
 	console.log("submit button pressed");
-	var start1 = document.getElementById("start-1").value;
-	var length1 = document.getElementById("length-1").value;
-	console.log("job 1: start: " + start1 + " length: " + length1);
+	var jobsList = document.getElementById("jobs");
+	jobs = [];
+	
+	for(job of jobsList.children) {
+		var name = job.children[0].innerText;
+		var start = job.children[2].value;
+		var length = job.children[4].value;
+		jobs.push({
+			name: name,
+			start: start,
+			length, length
+		});
+	}
+	console.log(jobs);
 }
 
 function makeBlock() {
 	// returns a div of a certain width + color
 }
+
+// write a function that takes in an array of job objects
+// and returns the time periods each job ran for
 
