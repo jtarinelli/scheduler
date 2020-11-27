abef1c48d67fadcd981086351c9acc25f1f80b38
unknown <juliatarinelli4@gmail.com>
2020-11-27 15:35:33 -0500

    Average turnaround and response time added

--- output.js
+++ output.js
@@ -1,9 +1,11 @@
-// functions to read in input, manage the actual simulation, and generate output
+// functions to read in input, run the actual simulation, and generate output
 
 // runs when submit/go button is clicked
 function submitButton() {
-	jobs = readInJobs();
-	FIFO(jobs);
+	var jobs = readInJobs();
+	var blocks = FIFO(jobs);
+	generateSimulation(blocks);
+	generateStats(jobs);
 }
 
 // reads in input from the form and returns a list of job objects
@@ -14,46 +16,53 @@ function readInJobs() {
 	
 	for(job of jobsList.children) {
 		var name = job.children[0].innerText;
-		var start = Number(job.children[2].value);
+		var arrival = Number(job.children[2].value);
 		var length = Number(job.children[4].value);
 		jobs.push({
 			name: name,
-			start: start,
-			length, length
+			arrival: arrival,
+			length, length,
+			start: -1, 
+			finish: -1
 		});
 	}
 	return jobs;
 }
 
+// reads in a list of jobs and returns a list of blocks (as in blocks of time when each job runs)
+// using the first in first out (FIFO) algorithm
 function FIFO(jobs) {	
-	jobs.sort((a, b) => a.start - b.start); // check about compatability of arrow functions
+	jobs.sort((a, b) => a.arrival - b.arrival); // check about compatability of arrow functions
 	var time = 0;
-	var blocks = []; // idk what else to call this
+	var blocks = []; 
 	
 	for (job of jobs) {
-		// check for gaps between jobs
-		if ((blocks.length == 0 && job.start != 0) || ((blocks[blocks.length-1].start + blocks[blocks.length-1].length) < job.start)) {
+		// check for gaps between jobs, or between time 0 and the first job
+		if ((blocks.length == 0 && job.arrival != 0) || (blocks.length > 0 && (blocks[blocks.length-1].start + blocks[blocks.length-1].length) < job.arrival)) {
 			blocks.push({
 				name: "Empty",
 				start: time,
-				length: job.start - time
+				length: job.arrival - time
 			});
-			time = job.start;
+			time = job.arrival;
 		}
-		
+
+		// run the whole job
+		job.start = time;
 		blocks.push({
 			name: job.name,
 			start: time,
 			length: job.length
 		});
 		time += job.length;
+		job.finish = time;
 	}
 	
-	generateOutput(blocks);
+	return blocks;
 }
 
 // takes an array of blocks and makes it into nodes and puts them on the page (terrible description)
-function generateOutput(blocks) {
+function generateSimulation(blocks) {
 	var output = document.getElementById("output");
 	output.innerHTML = ""; // controversial way to clear all children
 	
@@ -61,6 +70,25 @@ function generateOutput(blocks) {
 		var newBlock = makeBlockNode(block);
 		output.appendChild(newBlock);
 	}
+}
+
+// calculates the average response and turnaround time and adds them to the end of the output
+function generateStats(jobs) {
+	var output = document.getElementById("output");
+	var turnaroundTotal = 0;
+	var responseTotal = 0;
+	
+	for (job of jobs) {
+		turnaroundTotal += (job.finish - job.arrival);
+		responseTotal += (job.start - job.arrival);
+	}
+	
+	var averageTurnaround = turnaroundTotal / jobs.length;
+	var averageResponse = responseTotal / jobs.length;
+	
+	var averages = document.createElement("div");
+	averages.innerHTML = "Average Turnaround Time: " + averageTurnaround + "</br>Average Response Time: " + averageResponse;
+	output.appendChild(averages);
 }
 
 // takes in a block object and returns a block node
@@ -72,7 +100,7 @@ function makeBlockNode(blockObj) {
 }
 
 function roundRobin(jobs, quantum) {
-	jobs.sort((a, b) => a.start - b.start); 
+	jobs.sort((a, b) => a.arrival - b.arrival); 
 	var i = 0;
 	
 }
