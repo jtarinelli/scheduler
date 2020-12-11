699bec70ae7683d11eb87b267a5e2462ce0651ec
unknown <juliatarinelli4@gmail.com>
2020-12-11 10:55:15 -0500

    Add randomInt function

--- scripts/input.js
+++ scripts/input.js
@@ -3,8 +3,7 @@ let numJobs = 1;
 let minJobs = 1;
 let maxJobs = 10;
 
-// should probably just do this with css classes now that i think about it
-//let colors = ["blue", "red", "yellow", "pink", "purple", "green", "magenta", "teal", "violet", "gray"];
+// background colors in order of which job they correspond to
 let colors = ["#FF9AA2", "#FFB7B2", "#FFDAC1", "#fffecf", "#E2F0CB", "#B5EAD7", "#c5fcfb", "#d5cfff", "#fad2f7", "#eeeeee"];
 
 function addJob() {
@@ -47,7 +46,7 @@ function changeJobNumber(job, number) {
 	job.children[9].setAttribute("onclick","removeJob(" + number + ")");
 }
 
-// sets the arrival time and length of a job
+// change the values of a job's fields
 function setJobValues(job, arrival, length, ioFreq, ioLength) {
 	job.children[2].value = arrival;
 	job.children[4].value = length;
@@ -55,7 +54,7 @@ function setJobValues(job, arrival, length, ioFreq, ioLength) {
 	job.children[8].value = ioLength;
 }
 
-// sets the arrival and length of every job to a random value
+// sets the values every job to a random value
 function randomize() {
 	let jobsList = document.getElementById("jobs").children;
 	arrivalMin = 0;
@@ -68,10 +67,15 @@ function randomize() {
 	ioLengthMax = 10;
 	
 	for (job of jobsList) {
-		let arrival = Math.floor(Math.random() * (arrivalMax - arrivalMin) ) + arrivalMin;
-		let length = Math.floor(Math.random() * (lengthMax - lengthMin) ) + lengthMin;
-		let ioFreq = Math.floor(Math.random() * (ioFreqMax - ioFreqMin) ) + ioFreqMin;
-		let ioLength = Math.floor(Math.random() * (ioLengthMax - ioLengthMin) ) + ioLengthMin;
+		let arrival = randomInt(arrivalMin, arrivalMax);
+		let length = randomInt(lengthMin, lengthMax);
+		let ioFreq = randomInt(ioFreqMin, ioFreqMax);
+		let ioLength = randomInt(ioLengthMin, ioLengthMax);
 		setJobValues(job, arrival, length, ioFreq, ioLength);
 	}
+}
+
+// return a random integer between min and max
+function randomInt(min, max) {
+	return Math.floor(Math.random() * (max - min) ) + min;
 }
